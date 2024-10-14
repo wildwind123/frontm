861dd586fcacf106ce6fc65bdfcdab65a14d4890
Petr <vinokurovvin@list.ru>
2024-10-14 10:54:33 +0900

    icons

---
+++ icons/IconPlace.vue
@@ -0,0 +1,6 @@
+<template>
+    <Icon icon="ic:outline-place" class="h-4 w-4" />
+</template>
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+</script>
---
+++ icons/index.ts
@@ -0,0 +1,5 @@
+import IconPlace from '#src/frontm/icons/IconPlace.vue'
+
+export const Icons = {
+  Place: IconPlace,
+}
