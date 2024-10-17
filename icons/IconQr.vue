4c786ebdd2acb2afbcb9f8526285d49cfa7d56d8
Petr <vinokurovvin@list.ru>
2024-10-17 21:53:17 +0900

    IconQr.vue

---
+++ icons/IconQr.vue
@@ -0,0 +1,6 @@
+<template>
+  <Icon icon="mdi:qrcode-scan" class="h-4 w-4" />
+</template>
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue'
+</script>
--- icons/index.ts
+++ icons/index.ts
@@ -1,5 +1,7 @@
 import IconPlace from '#src/frontm/icons/IconPlace.vue'
+import IconQr from '#src/frontm/icons/IconQr.vue'
 
 export const Icons = {
   Place: IconPlace,
+  Qr: IconQr,
 }
